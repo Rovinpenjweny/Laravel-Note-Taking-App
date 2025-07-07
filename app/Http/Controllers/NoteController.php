1b7d560fecffdaf09f8fdb25c48e39588184fde1
Rovin <rovin.7813906@gmail.com>
2025-07-07 07:20:56 -0700

    Creating controller for notes

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('note.index');
+    }
+
+
+    // for showing individual items
+    public function show(Note $note)
+    {   
+        return view('note.show' , compact('note'));
+    }
+    
+    public function store()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        return view('note.create');
+    }
+
+
+    public function edit(Note $note)
+    {
+        return view('note.edit' , compact('note'));
+    }
+
+
+    public function update(Note $note)
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/notes' , [NoteController::class , 'index'])->name('note.index');
+Route::get('/notes/create' , [NoteController::class , 'create'])->name('note.create');
+Route::post('/notes' , [NoteController::class , 'store'])->name('note.store');
+Route::get('/notes/{id}' , [NoteController::class , 'show'])->name('note.show');
+Route::get('/notes/{id}/edit' , [NoteController::class , 'edit'])->name('note.edit');
+Route::delete('/notes/{id}' , [NoteController::class , 'destroy'])->name('note.destroy');
